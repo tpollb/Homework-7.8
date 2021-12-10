bda4b99087a6d6dd5cebafc8d7e6a095baa9b821
Сергей Усков <q3mydoom@hotmail.com>
2021-12-10 16:16:38 +0500

    Добавьте файлы проекта.

---
+++ Employee.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Homework_7._8
+{
+    public struct Employee : IEnumerable
+    {
+        /// <summary>
+        /// инкапсулированные данные
+        /// </summary>
+        private int id;
+        private DateTime recdate;
+        private DateTime rectime;
+        private string fio;
+        private int age;
+        private int height;
+        private DateTime dob;
+        private string homeTown;
+
+        public DateTime DoB { get => dob; set => dob = value; }
+        public string HomeTown { get => homeTown; set => homeTown = value; }
+        public int Height { get => height; set => height = value; }
+        public int Age { get => age; set => age = value; }
+        public string Fio { get => fio; set => fio = value; }
+        public DateTime RecDate { get => recdate; set => recdate = value; }
+        public DateTime RecTime { get => rectime; set => rectime = value; }
+        public int Id { get => id; set => id = value; }
+
+        /// <summary>
+        /// Метод создания
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="recdate"></param>
+        /// <param name="fio"></param>
+        /// <param name="age"></param>
+        /// <param name="height"></param>
+        /// <param name="dob"></param>
+        /// <param name="homeTown"></param>
+        public Employee(int id, DateTime recdate, DateTime rectime, string fio, int age, int height, DateTime dob, string homeTown)
+        {
+            this.id = id;
+            this.recdate = recdate;
+            this.rectime = rectime;
+            this.fio = fio;
+            this.age = age;
+            this.height = height;
+            this.dob = dob;
+            this.homeTown = homeTown;
+        }
+
+        /// <summary>
+        /// Метод чтения данных
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public string GetField(int i)//Извлекаем данные
+        {
+            if (i == 0) return id.ToString();
+            if (i == 1) return recdate.ToString();
+            if (i == 2) return rectime.ToString();
+            if (i == 3) return fio;
+            if (i == 4) return age.ToString();
+            if (i == 5) return height.ToString();
+            if (i == 6) return dob.ToString();
+            if (i == 7) return homeTown;
+            return "";
+        }
+        /// <summary>
+        /// Метод записи
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="str"></param>
+        public void SetField(int i, string str)//Помещаем данные
+        {
+            if (i == 0) id = int.Parse(str);
+            else if (i == 1) recdate = DateTime.Parse(str);
+            else if (i == 2) rectime = DateTime.Parse(str);
+            else if (i == 3) fio = str;
+            else if (i == 4) age = int.Parse(str);
+            else if (i == 5) height = int.Parse(str);
+            else if (i == 6) dob = DateTime.Parse(str);
+            else if (i == 7) homeTown = str;
+        }
+
+        /// <summary>
+        /// Метод вывода данных на экран
+        /// </summary>
+        /// <returns></returns>
+        public string Print()
+        {
+            return $"id: {id} Дата записи: " +
+                $"{recdate.ToShortDateString(), -6}" +
+                $" Время записи: {rectime.ToShortTimeString(),-6}" +
+                $" ФИО: {fio, -30}" +
+                $" Возраст: {age, -6}" +
+                $" Дата рождения {dob.ToShortDateString(), -6}" +
+                $" Место рождения {homeTown, -6}";
+        }
+        /// <summary>
+        /// Метод возврата данных для записи в файл
+        /// </summary>
+        /// <returns></returns>
+        public string ReturnValues()
+        {
+            return $"{id}#{recdate.ToShortDateString()}#{rectime.ToShortTimeString()}#{fio}#{age}#{height}#{dob.ToShortDateString()}#{homeTown}";
+        }
+        /// <summary>
+        /// хз зачем, в процессе игрался
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace Homework_7._8
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Метод геренрации ID. Считывает последний из файла базы данных и увеличивает на 1.
+        /// </summary>
+        /// <param name="path">Путь к файлу</param>
+        /// <returns></returns>
+        static public int IdGenetrate(string path)
+        {
+
+            string[] str = File.ReadAllLines(path);
+            if (str.Length == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                String last = File.ReadLines(path).Last();
+                string[] lastArr = last.Split(new char[] { '#' });
+                int id = int.Parse(lastArr[0]);
+                id++;
+                return id;
+            }
+        }
+
+        /// <summary>
+        /// Метод получения списка обьектов структуры из файла
+        /// </summary>
+        /// <param name="FileName"></param>
+        /// <returns></returns>
+        public static List<Employee> GetStructList(string FileName)
+        {
+            int i = default;
+            int RowsNumber = 8; //количество полей
+            int count = File.ReadAllLines(FileName).Length; //количество строк в файле
+            string[] arr = new string[count]; //массив строк из файла
+            string[] words = new string[count];
+
+            List<Employee> db = new();
+            Employee emp = new();
+
+            using (StreamReader sr = new(FileName, System.Text.Encoding.Default))
+            {
+                string line;
+                //читаем файл до конца
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //заполнили массив строк из файла
+                    arr[i] = line;
+                    i++;
+                }
+
+                for (i = 0; i < count; i++)
+                {
+                    int j;
+                    for (j = 0; j < RowsNumber; j++)
+                    {
+                        //разделяем строку по #
+                        words = arr[i].Split(new char[] { '#' });
+                        //присваиваем елементу структуры emp разделённые значения
+                        emp.SetField(j, words[j]);
+                    }
+                    //добавляем новый элемент списка 
+                    db.Add(emp);
+                }
+                sr.Close();
+            }
+            return db;
+        }
+        
+        /// <summary>
+        /// Метод вывода списка структур
+        /// </summary>
+        /// <param name="db"></param>
+        /// <param name="count"></param>
+        public static void DisplayList(List<Employee> db, int count)
+        {
+            for (int i = 0; i < count - 1; i++)
+            {
+                Console.Write($"{db[i].Print()}");
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Метод перезаписи файла с изменениями. 
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <param name="data">Список</param>
+        static public void WriteListToFile(string filepath, List<Employee> data)
+        {
+            using (StreamWriter sw = new StreamWriter(filepath, false, System.Text.Encoding.Default))
+            {
+                for (int i = 0; i < data.Count; i++)
+                {
+                    sw.WriteLine(data[i].ReturnValues());
+                }
+                sw.Close();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            bool flag = false;
+            bool flag1 = false;
+            int Choice = 0;
+            char key = 'y';
+            string FileName = @"db.txt";
+            int LinesCount = File.ReadAllLines(FileName).Length; //количество строк в файле
+
+            List<Employee> db = new ();
+            Employee emp = new();
+
+            int id = 1;
+            string input;
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("База данных «Сотрудники»");
+            Console.ResetColor();
+            Console.WriteLine($"Загружаем файл {FileName}:");
+
+            //загружаем данные из файла, если его нет - создаём
+            if (!File.Exists(FileName)) //проверка наличия файла
+            {
+                Console.WriteLine($"Файл {FileName} не существует.\nСоздаём файл {FileName}");
+                File.Create(FileName);
+                Console.WriteLine();
+            }
+            else //считываем файл, записываем данные в структуру
+            {
+                db = GetStructList(FileName);
+                Console.WriteLine($"Файл {FileName} загружен.\nКоличество записей {LinesCount}");
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+
+            do
+            {
+                while (flag == false || Choice < 1 || Choice > 7)
+                {
+
+                    Console.WriteLine("Выберите действие 1 - 7:");
+                    Console.WriteLine("1) Просмотр записи");
+                    Console.WriteLine("2) Создание записи");
+                    Console.WriteLine("3) Удаление записи");
+                    Console.WriteLine("4) Редактирование записи");
+                    Console.WriteLine("5) Загрузка записей в выбранном диапазоне дат");
+                    Console.WriteLine("6) Сортировка по возрастанию и убыванию даты");
+                    Console.WriteLine("7) Выход из программы");
+
+                    flag = int.TryParse(Console.ReadLine(), out Choice);
+
+                    //Просмотр записи
+                    if (Choice == 1)
+                    {
+                        while (flag1 == false)
+                        {
+                            Console.WriteLine($"Ведите номер записи для отображения от 1 до {LinesCount}:");
+                            flag1 = int.TryParse(Console.ReadLine(), out id);
+                        }
+                        
+                        Console.WriteLine(db[id - 1].Print());
+                        flag = false;
+                        flag1 = false;
+                    }
+
+                    //Создание записи
+                    if (Choice == 2)
+                    {
+                        
+                            Console.WriteLine($"Введите ФИО: ");
+                            string fio = $"{Console.ReadLine()}";
+
+                            //проверка возраста
+                            flag = default;
+                            int age = default;
+                            while (flag == false || age < 1 || age > 120)
+                            {
+                                Console.WriteLine("Введите возраст (1 - 120): ");
+                                input = Console.ReadLine();
+                                flag = int.TryParse(input, out age);
+                            }
+
+                            //проверка роста
+                            flag = default;
+                            int height = default;
+                            while (flag == false || height < 10 || height > 230)
+                            {
+                                Console.WriteLine("Введите рост (10 - 230): ");
+                                input = Console.ReadLine();
+                                flag = int.TryParse(input, out height);
+                            }
+
+                            //проверка даты рождения
+                            DateTime dob;
+                            string date;
+                            do
+                            {
+                                Console.WriteLine("Введите дату рождения в формате дд.ММ.гггг (день.месяц.год):");
+                                date = Console.ReadLine();
+                            }
+                            while (!DateTime.TryParseExact(date, "dd.MM.yyyy", null, DateTimeStyles.None, out dob));
+
+                            Console.WriteLine($"Введите место рождения: ");
+                            string hometown = $"{Console.ReadLine()}";
+
+                        //WriteFile(FileName, GenerateOutString(IdGenetrate(FileName), fio, age, height, dob, hometown));
+                        
+                        emp.Id = IdGenetrate(FileName);
+                        emp.RecDate = DateTime.Now;
+                        emp.RecTime = DateTime.Now;
+                        emp.Fio = fio;
+                        emp.Age = age;
+                        emp.Height = height;
+                        emp.DoB = dob;
+                        emp.HomeTown = hometown;
+
+                        db.Add(emp);
+                        //DisplayList(db, IdGenetrate(FileName) + 1);
+                        
+                        flag = false;
+                    }
+
+                    //Удаление записи
+                    if (Choice == 3)
+                    {
+                        while (flag1 == false)
+                        {
+                            Console.WriteLine($"Ведите номер записи для удаления от 1 до {LinesCount}:");
+                            flag1 = int.TryParse(Console.ReadLine(), out id);
+                        }
+                        db.RemoveAt(id);
+                        flag = false;
+                        flag1 = false;
+                    }
+
+
+                    //Редактирование записи
+                    if (Choice == 4)
+                    {
+                        while (flag1 == false)
+                        {
+                            Console.WriteLine($"Ведите номер записи для редактирования от 1 до {LinesCount}:");
+                            flag1 = int.TryParse(Console.ReadLine(), out id);
+                        }
+                        flag = false;
+                    }
+
+                    //Загрузка записей в выбранном диапазоне дат
+                    if (Choice == 5)
+                    {
+
+                        flag = false;
+                    }
+
+                    //Сортировка по возрастанию и убыванию даты
+                    if (Choice == 6)
+                    {
+                        Console.WriteLine($"Сортировать по возрастанию дат - 1, по убыванию - 2");
+                        while (flag == false || Choice < 1 || Choice > 2)
+                        {
+                            flag = int.TryParse(Console.ReadLine(), out Choice);
+                        }
+                        if (Choice == 1)
+                        {
+                            db.Sort((x, y) => x.RecDate.CompareTo(y.RecDate));
+                            DisplayList(db, LinesCount);
+                        } else
+                        {
+                            //db.OrderByDescending(x => x.RecDate);
+                            db.Sort((x, y) => y.RecDate.CompareTo(x.RecDate));
+                            DisplayList(db, LinesCount);
+                        }
+                        flag = false;
+                    }
+
+                    //Выход из программы
+                    if (Choice == 7)
+                    {
+                        Process.GetCurrentProcess().Kill();
+                    }
+
+                    Console.WriteLine($"Хотите записать изменения в файл {FileName}? y/n");
+                    key = Char.ToLower(Console.ReadKey().KeyChar);
+                    if (key == 'y')
+                    {
+                        WriteListToFile(FileName, db);
+                    }
+
+                    Console.WriteLine("Хотите продолжить? y/n");
+                    key = Char.ToLower(Console.ReadKey().KeyChar);
+                    if (key == 'n')
+                    {
+                        Process.GetCurrentProcess().Kill();
+                    }
+                    Console.Clear();
+                }
+            } while (key != 'n');
+        }
+    }
+}
+
